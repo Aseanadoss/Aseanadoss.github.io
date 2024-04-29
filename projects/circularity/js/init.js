7b1e5cf470261c47078ebf6cafa9c60408d9ea9e
Aseana Doss <aseana.doss@jpschools.org>
2024-04-29 18:38:22 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle; // variable that will hold a single circle when creating circles
+        var circles = []; // variable to store all circles in one Array
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle  () {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
+
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
+            
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -62,7 +72,11 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.y > canvas.height) {
+                
+                circle.y=0
+                
+                }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
